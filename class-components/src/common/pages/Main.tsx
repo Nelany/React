88ab83248bfae6439a11bc4ec46678c3ebde8a5c
Nelany <svetasweetka@gmail.com>
2024-07-02 22:03:21 +0200

    init: environment setup

---
+++ class-components/src/common/pages/Main.tsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+export default class Main extends Component {
+  state = {};
+
+  render() {
+    return <div>Main</div>;
+  }
+}
---
+++ class-components/src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import Main from './common/pages/Main';
+
+const rootElement = document.createElement('div');
+rootElement.id = 'root';
+document.body.appendChild(rootElement);
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>
+);
