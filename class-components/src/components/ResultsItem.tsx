716d02fabe8ec95740e3f0ee3edd1f01118e6459
Nelany <svetasweetka@gmail.com>
2024-07-11 19:21:49 +0200

    refactor: migrate ResultsItem component from class to function

--- class-components/src/components/ResultsItem.tsx
+++ class-components/src/components/ResultsItem.tsx
@@ -1,30 +1,21 @@
-import { Component } from 'react';
 import { Character } from '../types/types';
 
-type Props = {
+interface Props {
   name: string;
   character: Character;
-};
-
-export class ResultsItem extends Component<Props> {
-  constructor(props: Props) {
-    super(props);
-  }
-
-  render() {
-    const { character } = this.props;
-
-    return (
-      <div className="results-item">
-        <img className="result-img" src={character.image} alt="img" />
-        <div>
-          <h2>{this.props.name}</h2>
-          <h4>{`Status: ${character.status};`}</h4>
-          <h4>{`Species: ${character.species};`}</h4>
-          <h4>{`${character.type && `Type: ${character.type};`}`}</h4>
-          <h4>{`Last known location: ${character.location.name};`}</h4>
-        </div>
-      </div>
-    );
-  }
 }
+
+export const ResultsItem = ({ name, character }: Props) => {
+  return (
+    <div className="results-item">
+      <img className="result-img" src={character.image} alt="img" />
+      <div>
+        <h2>{name}</h2>
+        <h4>{`Status: ${character.status}`}</h4>
+        <h4>{`Species: ${character.species}`}</h4>
+        {character.type && <h4>{`Type: ${character.type}`}</h4>}
+        <h4>{`Last known location: ${character.location.name}`}</h4>
+      </div>
+    </div>
+  );
+};
