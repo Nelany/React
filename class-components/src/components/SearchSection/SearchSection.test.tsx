f5bb55c9a4ba78a0734f19aa02a13d383df9f69b
Nelany <svetasweetka@gmail.com>
2024-07-13 21:30:32 +0200

    feat: add tests for SearchSection

--- class-components/src/components/SearchSection/SearchSection.test.tsx
+++ class-components/src/components/SearchSection/SearchSection.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { describe, beforeEach, it, expect, vi } from 'vitest';
 import { SearchSection } from './SearchSection';
@@ -48,5 +48,51 @@ describe('SearchSection', () => {
     fireEvent.change(input, { target: { value: 'test' } });
 
     expect(input.value).toBe('test');
+  });
+
+  it('saves input value to local storage on search button click', async () => {
+    render(
+      <MemoryRouter>
+        <SearchSection
+          setCharactersFromResponse={setCharactersFromResponse}
+          setIsLoading={setIsLoading}
+          setIfNextPage={setIfNextPage}
+        />
+      </MemoryRouter>
+    );
+
+    const input = screen.getByPlaceholderText(
+      'Enter text...'
+    ) as HTMLInputElement;
+    fireEvent.change(input, { target: { value: 'test' } });
+
+    const button = screen.getByText('Search!');
+    fireEvent.click(button);
+
+    await new Promise((resolve) => setTimeout(resolve, 100));
+
+    await waitFor(() => {
+      expect(localStorage.getItem('searchQuery')).toBe('test');
+    });
+  });
+
+  it('fetches input value from local storage on component mount', () => {
+    localStorage.setItem('searchQuery', 'initial value');
+
+    render(
+      <MemoryRouter>
+        <SearchSection
+          setCharactersFromResponse={setCharactersFromResponse}
+          setIsLoading={setIsLoading}
+          setIfNextPage={setIfNextPage}
+        />
+      </MemoryRouter>
+    );
+
+    const input = screen.getByPlaceholderText(
+      'Enter text...'
+    ) as HTMLInputElement;
+
+    expect(input.value).toBe('initial value');
   });
 });
