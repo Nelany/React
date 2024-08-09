7a6f2abb27a9d759dfa8af61b97b0759b1b65a95
Nelany <svetasweetka@gmail.com>
2024-08-09 17:03:04 +0200

    refactor: update error handling

--- class-components/src/components/PageProvider/PageProvider.tsx
+++ class-components/src/components/PageProvider/PageProvider.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../../store/store';
 import { ThemeProvider } from '../../ThemeContext/ThemeContext';
+import { ErrorBoundaryWrapper } from '../ErrorBoundary/ErrorBoundaryWrapper';
 import Main from '../Main/Main';
 
 interface PageProps {
@@ -11,9 +12,11 @@ interface PageProps {
 const PageProvider = ({ children }: PageProps) => {
   return (
     <ThemeProvider>
-      <Provider store={store}>
-        <Main>{children}</Main>
-      </Provider>
+      <ErrorBoundaryWrapper>
+        <Provider store={store}>
+          <Main>{children}</Main>
+        </Provider>
+      </ErrorBoundaryWrapper>
     </ThemeProvider>
   );
 };
